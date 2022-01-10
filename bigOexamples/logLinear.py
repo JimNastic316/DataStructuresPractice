a1bbf075cef0014bef977b13955d60a18f09874b
JimNastic316 <JimNastic316@gmail.com>
2022-01-10 15:31:08 -0500

    BigOExamples
    Common complexities

---
+++ bigOexamples/logLinear.py
@@ -0,0 +1,23 @@
+# '''
+# This code sample demonstrates splitting an input in half
+# at every step of a loop. We see this in things like binary sarch
+# or some kinds of sorts.
+# '''
+
+from timethis import timethis
+import time
+
+@timethis
+def split_in_two(i):
+    for j in range (i):
+        k=i
+        while k > 1:
+            k = k/2
+            time.sleep(0.01) #We need to insert an arbitrary delay otherwise it is too fast
+
+split_in_two(50)
+split_in_two(100)
+split_in_two(200)
+split_in_two(400)
+split_in_two(800)
+split_in_two(1600)
